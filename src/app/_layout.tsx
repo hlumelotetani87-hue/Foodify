b924770f9fa054e5a2e4d4caed7a3f94284ab49d
hlumelotetani87-hue <hlumelotetani87@gmail.com>
2026-03-19 18:03:23 +0200

    chore: update dependencies and add Tailwind CSS support
    - Updated expo and related packages to latest versions.
    - Added nativewind and tailwindcss for styling.
    - Created tailwind.config.js for Tailwind CSS configuration.
    - Modified _layout.tsx to include global CSS and updated App export.
    - Updated tsconfig.json to include nativewind-env.d.ts.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
-
+import "./global.css"
+ 
+export default App() {
+  /* Your App */
+}
 export default function RootLayout() {
   return <Stack screenOptions={{headerShown:false}}/>;
 }
